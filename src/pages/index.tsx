97ac787be5b486127d896e925ffad17aac74892b
matveyb <matvey.b2022@gmail.com>
2024-04-16 19:36:38 +0200

    refactoring

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,18 +2,43 @@ import Image from "next/image";
 import styles from "./index.module.scss";
 import Container from "@/components/layout/container";
 import Button from "@/components/button";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 import MainPreview from "@/assets/common/preview_main.png";
+import Footer from "@/assets/common/footer.png";
 import Slide from "@/assets/common/slide.png";
 import Card1 from "@/assets/common/card1.png";
 import Card2 from "@/assets/common/card2.png";
 import Card3 from "@/assets/common/card3.png";
 import Card4 from "@/assets/common/card4.png";
 
+import road1 from "@/assets/common/roadmap1.png";
+import road2 from "@/assets/common/roadmap2.png";
+import road3 from "@/assets/common/roadmap1.png";
+
+const roadmapSlider = [
+  {
+    img: road1,
+    text: "Top-100 token holders will get their very own Kombutt via the Kombutt Generator!",
+    title: "Q2 2024"
+  },
+  {
+    img: road2,
+    text: "The Kombutt Manga with epic tales for the top-50 token holders!",
+    title: "Q3 2024"
+  },
+  {
+    img: road3,
+    text: "Top-100 token holders will get their very own Kombutt via the Kombutt Generator!",
+    title: "Q4 2024"
+  }
+];
+
 const Home = () => {
   return (
-    <div className="w-full mb-20">
-      <div className="w-full">
+    <div className="w-full">
+      <div className="w-full mb-40">
         <Container>
           <div className={styles.content}>
             <h1>Choose Your Destiny</h1>
@@ -66,8 +91,58 @@ const Home = () => {
       <Container>
         <div className={styles.roadmap}>
           <h1>Fatality Roadmap</h1>
+          <Swiper spaceBetween={20} slidesPerView={2} freeMode>
+            {roadmapSlider.map((_, key) => (
+              <SwiperSlide>
+                <div className={styles.slideMain}>
+                  <Image
+                    className={key === 1 ? "bg-black" : ""}
+                    alt={_.title}
+                    src={_.img}
+                  />
+                  <h2 className="mt-10">{_.title}</h2>
+                  <p>{_.text}</p>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
       </Container>
+
+      <div className={styles.footer}>
+        <Image className={styles.footerImg} alt="main" src={Footer} />
+        <div className={styles.footerLinks}>
+          <Container>
+            <div className={styles.linkItem}>
+              <h2>social links:</h2>
+              <div className="flex justify-center items-center gap-2 mt-2">
+                <span>1.</span>
+                <a target="_blank" className="link" href="test">
+                  telegram
+                </a>
+              </div>
+            </div>
+            <div className={styles.linkItem}>
+              <h2>root-contract:</h2>
+              <div className="flex justify-center items-center gap-2 mt-2">
+                <span>1.</span>
+                <a target="_blank" className="link" href="test">
+                  link
+                </a>
+              </div>
+            </div>
+            <div className={styles.linkItem}>
+              <h2>venom-scan:</h2>
+              <div className="flex justify-center items-center gap-2 mt-2 mt-2">
+                <span>1.</span>
+                <a target="_blank" className="link" href="test">
+                  link
+                </a>
+              </div>
+            </div>
+          </Container>
+        </div>
+      </div>
     </div>
   );
 };
