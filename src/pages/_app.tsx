cf4dbed35d786c9acb564e796a25a551a21e7adb
matveyb <matvey.b2022@gmail.com>
2024-04-17 20:56:56 +0200

    fix prettier

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,11 +19,14 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Kombutt" />
         <meta property="og:site_name" content="Kombutt" />
-        <meta property="og:description" content="Kombutt.io - Join Kombutt in the ultimate crypto combat!" />
+        <meta
+          property="og:description"
+          content="Kombutt.io - Join Kombutt in the ultimate crypto combat!"
+        />
         <meta property="og:image" content="https://kombutt.wtf/meta-img.png" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="631" />
-    
+
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="https://kombutt.wtf" />
         <meta property="twitter:url" content="https://kombutt.wtf" />
@@ -33,7 +36,6 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
           content="Kombutt.io - Join Kombutt in the ultimate crypto combat!"
         />
         <meta name="twitter:image" content="https://kombutt.wtf/meta-img.png" />
-    
       </Head>
 
       <RenderLayout>
