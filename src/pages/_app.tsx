afa326df0ece763bb2d8188dfdff20ed5a525c90
matveyb <matvey.b2022@gmail.com>
2024-04-17 20:53:21 +0200

    meta fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,6 +14,26 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1"
         />
+
+        <meta property="og:url" content="%REACT_APP_ORIGIN%" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Kombutt" />
+        <meta property="og:site_name" content="Kombutt" />
+        <meta property="og:description" content="Kombutt.io - Join Kombutt in the ultimate crypto combat!" />
+        <meta property="og:image" content="%REACT_APP_ORIGIN%/meta-img.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="631" />
+    
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="%REACT_APP_ORIGIN%" />
+        <meta property="twitter:url" content="%REACT_APP_ORIGIN%" />
+        <meta name="twitter:title" content="Kombutt" />
+        <meta
+          name="twitter:description"
+          content="Kombutt.io - Join Kombutt in the ultimate crypto combat!"
+        />
+        <meta name="twitter:image" content="%REACT_APP_ORIGIN%/meta-img.png" />
+    
       </Head>
 
       <RenderLayout>
