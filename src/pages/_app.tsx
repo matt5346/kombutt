32e0b828d89fde78a8da25650854735d1128b42c
matveyb <matvey.b2022@gmail.com>
2024-04-17 20:54:42 +0200

    fix meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,24 +15,24 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
           content="width=device-width, initial-scale=1, maximum-scale=1"
         />
 
-        <meta property="og:url" content="%REACT_APP_ORIGIN%" />
+        <meta property="og:url" content="https://kombutt.wtf" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Kombutt" />
         <meta property="og:site_name" content="Kombutt" />
         <meta property="og:description" content="Kombutt.io - Join Kombutt in the ultimate crypto combat!" />
-        <meta property="og:image" content="%REACT_APP_ORIGIN%/meta-img.png" />
+        <meta property="og:image" content="https://kombutt.wtf/meta-img.png" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="631" />
     
         <meta name="twitter:card" content="summary_large_image" />
-        <meta property="twitter:domain" content="%REACT_APP_ORIGIN%" />
-        <meta property="twitter:url" content="%REACT_APP_ORIGIN%" />
+        <meta property="twitter:domain" content="https://kombutt.wtf" />
+        <meta property="twitter:url" content="https://kombutt.wtf" />
         <meta name="twitter:title" content="Kombutt" />
         <meta
           name="twitter:description"
           content="Kombutt.io - Join Kombutt in the ultimate crypto combat!"
         />
-        <meta name="twitter:image" content="%REACT_APP_ORIGIN%/meta-img.png" />
+        <meta name="twitter:image" content="https://kombutt.wtf/meta-img.png" />
     
       </Head>
 
