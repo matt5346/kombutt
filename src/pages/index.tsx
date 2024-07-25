e26d1bc9ed46297bf290cfd4cdea0a1ec0942fa8
matveyb <matvey.b2022@gmail.com>
2024-07-26 01:38:05 +0700

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -291,17 +291,19 @@ const Home = () => {
                     </a>
                   </div>
                 </div>
-                <h2>NFT collection:</h2>
-                <div className="flex justify-center items-center gap-2 mt-2">
-                  <span>1.</span>
-                  <a
-                    target="_blank"
-                    className="link"
-                    href="https://oasis.gallery/collection/0:8d50938251f2f7bd75780c6814aa7fefd305ebdecd5132083134d0f4e37c1bb0"
-                    rel="noreferrer"
-                  >
-                    NFT collections
-                  </a>
+                <div className={styles.linkItem}>
+                  <h2>NFT collection:</h2>
+                  <div className="flex justify-center items-center gap-2 mt-2">
+                    <span>1.</span>
+                    <a
+                      target="_blank"
+                      className="link"
+                      href="https://oasis.gallery/collection/0:8d50938251f2f7bd75780c6814aa7fefd305ebdecd5132083134d0f4e37c1bb0"
+                      rel="noreferrer"
+                    >
+                      NFT collections
+                    </a>
+                  </div>
                 </div>
                 <div className={styles.linkItem}>
                   <h2>root-contract:</h2>
